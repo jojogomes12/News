4b1b3e2249e03429078305f65481ded1f06d9d4f
jojogomes12 <joaoecarol39@gmail.com>
2024-06-06 14:07:04 -0300

    Adiciona funcionalidade de pesquisa

--- pages/index.js
+++ pages/index.js
@@ -3,34 +3,40 @@ import Link from 'next/link';
 import axios from 'axios';
 import 'tailwindcss/tailwind.css';
 require('dotenv').config();
+
 export default function Home({ articles }) {
     const [currentPage, setCurrentPage] = useState(1);
+    const [searchTerm, setSearchTerm] = useState('');
     const itemsPerPage = 5;
 
-    // Ordenar os artigos por data de publicação
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
     articles.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
 
-    // Calcular o índice inicial e final da lista de notícias para exibir na página atual
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = currentPage * itemsPerPage;
 
-    // Filtrar a lista de notícias com base na página atual
-    const currentArticles = articles.slice(startIndex, endIndex);
+    const filteredArticles = articles.filter(article => {
+        return (
+            article.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            article.author.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            new Date(article.publishedAt).toLocaleDateString().includes(searchTerm)
+        );
+    });
 
-    // Calcular o número total de páginas
-    const totalPages = Math.ceil(articles.length / itemsPerPage);
+    const currentArticles = filteredArticles.slice(startIndex, endIndex);
+    const totalPages = Math.ceil(filteredArticles.length / itemsPerPage);
 
-    // Função para ir para a próxima página
     const nextPage = () => {
         setCurrentPage(prevPage => prevPage + 1);
     };
 
-    // Função para ir para a página anterior
     const prevPage = () => {
         setCurrentPage(prevPage => prevPage - 1);
     };
 
-    // Função para obter o nome do autor ou definir como "Não mencionado"
     const getAuthorName = (article) => {
         if (article.author && article.source && article.author === article.source.name) {
             return 'Não mencionado';
@@ -41,6 +47,13 @@ export default function Home({ articles }) {
     return (
         <div className="container mt-5 mx-auto max-w-4xl">
             <h1 className="mb-4 text-3xl font-semibold">Notícias de Elden Ring</h1>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                placeholder="Pesquisar por título, autor ou data"
+                className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 mb-4"
+            />
             <div className="grid gap-4">
                 {currentArticles.map((article, index) => (
                     <div key={index} className="h-full">
@@ -59,7 +72,7 @@ export default function Home({ articles }) {
             <div className="flex justify-between mt-8">
                 <button disabled={currentPage === 1} onClick={prevPage} className="px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-700 rounded-md focus:outline-none">Anterior</button>
                 <p className="text-gray-700">{currentPage} de {totalPages}</p>
-                <button disabled={endIndex >= articles.length} onClick={nextPage} className="px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-700 rounded-md focus:outline-none">Próxima</button>
+                <button disabled={endIndex >= filteredArticles.length} onClick={nextPage} className="px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-700 rounded-md focus:outline-none">Próxima</button>
             </div>
         </div>
     );
@@ -72,7 +85,8 @@ export async function getStaticProps() {
         from: '2023',
         sortBy: 'popularity',
         language: 'pt', // Adicionando o parâmetro para buscar apenas notícias em português
-        apiKey: process.env.NEWS_API_KEY    };
+        apiKey: process.env.NEWS_API_KEY
+    };
 
     try {
         const response = await axios.get(url, { params });
